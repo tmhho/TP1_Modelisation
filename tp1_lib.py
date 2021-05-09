3741b61eba3f7766fb966f9f9b8b04de2c88f927
tmhho <amiho@Hos-MacBook-2.local>
2021-05-09 18:17:46 +0200

    correct code

--- tp1_lib.py
+++ tp1_lib.py
@@ -44,22 +44,25 @@ def Uinit3(X,J):
 def schema1(U0,J,alpha):
     U1 = np.zeros(J)
     U1[1:] = U0[1:] - alpha*(U0[1:] - U0[0:-1])
-    U1[0] = U0[0] - alpha*(U0[0] - U0[-1])
+    U1[0] = U0[0] - alpha*(U0[0] - U0[-2])
     return U1
 
 def schema2(U0,J,alpha):
     U1 = np.zeros(J)
     U1[1:-1] = U0[1:-1] - alpha/2.*(U0[2:] - U0[0:-2]) + (alpha**2)/2.*(U0[2:] - 2.*U0[1:-1] + U0[0:-2])
-    U1[0] = U0[0] - alpha/2.*(U0[1] - U0[-1]) + (alpha**2)/2.*(U0[1] - 2.*U0[0] + U0[-1])
-    U1[-1] = U0[-1] - alpha/2.*(U0[0] - U0[-2]) + (alpha**2)/2.*(U0[0] - 2.*U0[-1] + U0[-2])
+    # U1[0] = U0[0] - alpha/2.*(U0[1] - U0[-1]) + (alpha**2)/2.*(U0[1] - 2.*U0[0] + U0[-1])
+    U1[0] = U0[0] - alpha/2.*(U0[1] - U0[-2]) + (alpha**2)/2.*(U0[1] - 2.*U0[0] + U0[-2])     
+    U1[-1] = U1[0]  
+    # U1[-1] = U0[-1] - alpha/2.*(U0[0] - U0[-2]) + (alpha**2)/2.*(U0[0] - 2.*U0[-1] + U0[-2])
     return U1
 
 def schema3(U0,J,alpha):
     U1 = np.zeros(J)
     U1[2:-1] = U0[2:-1] - alpha*(U0[2:-1] - U0[1:-2]) - alpha*(1-alpha)/4.*(U0[3:]-U0[2:-1]-U0[1:-2]+U0[0:-3])
-    U1[0] = U0[0] - alpha*(U0[0] - U0[-1]) - alpha*(1-alpha)/4.*(U0[1]-U0[0]-U0[-1]+U0[-2])
-    U1[1] = U0[1] - alpha*(U0[1] - U0[0]) - alpha*(1-alpha)/4.*(U0[2]-U0[1]-U0[0]+U0[-1])
-    U1[-1] = U0[-1] - alpha*(U0[-1] - U0[-2]) - alpha*(1-alpha)/4.*(U0[0]-U0[-1]-U0[-2]+U0[-3])
+    U1[0] = U0[0] - alpha*(U0[0] - U0[-2]) - alpha*(1-alpha)/4.*(U0[1]-U0[0]-U0[-2]+U0[-3])
+    U1[1] = U0[1] - alpha*(U0[1] - U0[0]) - alpha*(1-alpha)/4.*(U0[2]-U0[1]-U0[0]+U0[-2])
+    U1[-1] = U1[0]
+    # U1[-1] = U0[-1] - alpha*(U0[-1] - U0[-2]) - alpha*(1-alpha)/4.*(U0[0]-U0[-1]-U0[-2]+U0[-3])
     return U1
 
 def E1(xi, alpha):
@@ -69,7 +72,6 @@ def E2(xi, alpha):
 def E3(xi, alpha): 
     theta = xi/2.
     return 1-np.sqrt((1-2*alpha*(np.sin(theta)**2)*(1-(1-alpha)*(np.cos(theta)**2)))+ 4*(alpha**2)*(np.cos(theta)**2)*(1-(1-alpha)*(np.sin(theta)**2)))
-
 
 def Ephase1(xi, alpha):
     return alpha * xi - np.arccos((1-alpha*(1-np.cos(xi)))/(np.sqrt(1-2*alpha*(1-alpha)*(1-np.cos(xi)))))
